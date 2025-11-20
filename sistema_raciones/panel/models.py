aa8b47aebb1b48c21884abc5c30e0239558e377c
nicolas <toricolol12@gmail.com>
2025-11-19 22:29:32 -0300

    nuevo con retricciones

--- sistema_raciones/panel/models.py
+++ sistema_raciones/panel/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class Racion(models.Model):
+    nombre = models.CharField(max_length=100)
--- sistema_raciones/panel/views.py
+++ sistema_raciones/panel/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
+from django.shortcuts import redirect
+
 
 # LOGIN
 def login_view(request):
@@ -27,27 +30,45 @@ def logout_view(request):
     return redirect("login")
 
 
-# PÁGINAS PROTEGIDAS
 @login_required
+def login_view(request):
+    return render(request, "panel/login.html")
+
+
+# SOLO ADMIN PUEDE VER DASHBOARD
+@permission_required('panel.view_racion', raise_exception=True)
 def dashboard(request):
+    if not request.user.is_superuser:
+        return redirect("raciones")  # redirigir al trabajador
     return render(request, "panel/dashboard.html")
 
 
+# SOLO TRABAJADOR
 @login_required
+@permission_required('panel.view_racion', raise_exception=True)
 def raciones(request):
     return render(request, "panel/raciones.html")
 
 
+# SOLO ADMIN
 @login_required
 def proyecciones(request):
+    if not request.user.is_superuser:
+        return redirect("raciones")
     return render(request, "panel/proyecciones.html")
 
 
+# SOLO ADMIN
 @login_required
 def registros(request):
+    if not request.user.is_superuser:
+        return redirect("raciones")
     return render(request, "panel/registros.html")
 
 
+# SOLO ADMIN
 @login_required
 def configuraciones(request):
+    if not request.user.is_superuser:
+        return redirect("raciones")
     return render(request, "panel/configuraciones.html")
